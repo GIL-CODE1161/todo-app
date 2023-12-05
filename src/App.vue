b98729eda4fd7e0d893cc9754524923cf18ca759
george ocheme Onehlofiochi <onehlofiochiocheme@gmail.com>
2023-12-05 18:54:31 +0100

    todoapp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="title">TODO APP WITH VUE JS</h1>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <todo-list></todo-list>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
+    HelloWorld,
+    // eslint-disable-next-line vue/no-unused-components
+    TodoList,
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+  border: 1px solid black;
+  border-radius: 60px;
+  /* background-image: radial-gradient(black, white, black); */
+  background-image: linear-gradient(to left, white,black, white);
+  border-right-width: 10px;
+  border-left-width: 10px;
+  border-left-color: #65786B;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 24px;
 }
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+.title {
+  display: flex;
+  margin-left: 350px;
+}
+
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" 
+    placeholder="Input your todo" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+        <div class="todo-item-left">
+            <div v-if="!todo.editing" class="todo-item-label">{{ todo.title }}</div>
+            <input class="todo-item-edit" type="text" v-model="todo.title">
+        </div>
+        <div class="remove-item" @click="removeTodo(index)">
+            &times;
+          
+
+        </div>
+    </div>
+   </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'todo-list',
+    data () {
+        return {
+            newTodo:'',
+            idForTodo: 3,
+            todos: [
+                {
+                    'id':1,
+                    'completed': false,
+                    'title': 'Finish Vue App',
+                    'editing': false,
+                },
+                {
+                    'id':2,
+                    'completed': false,
+                    'title': 'Take Over World',
+                    'editing': false,
+                },
+                {
+                  'id':3,
+                    'completed': false,
+                    'title': 'Go to the saloon',
+                    'editing': false,
+                }
+            ]
+        }
+    },
+    methods: {
+        addTodo  (){
+            if (this.newTodo.trim().length == 0) {
+                return
+            }
+            this.todos.push ({
+                id: this.idForTodo,
+                title: this.newTodo,
+                completed: false,
+            })
+            this.newTodo = ''
+            this.idForTodo ++
+
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1)
+        }
+
+    }
+  }
+  </script>
+  
+  <style>
+  
+  .todo-input {
+    width: 350px;
+    padding: 10px 18px;
+    font-size: 18px;
+    font-style: italic;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    margin-bottom: 16px;
+    text-align: center;
+    display: inline;
+    margin-left: 15px;
+    
+
+  }
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+  }
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+  }
+  &:hover {
+    color: black;
+  }
+  .todo-item-left {
+    display: flex;
+    align-items: center;
+  }
+  .todo-item-label {
+    padding: 16px;
+    margin-left: 12px;
+    border: 1px solid white;
+    /* background-color: rgb(11, 68, 3); */
+    color: aqua;
+    border-top-right-radius: 25px;
+    border-bottom-left-radius: 25px;
+    background-image: linear-gradient(to left bottom,green,white);
+
+  }
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    background-color: lemonchiffon;
+  }
+  </style>
+  
